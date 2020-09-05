33d24708f0178913c35dab4cd53a10add0dd32ce
pierre <pierrendomo@yahoo.fr>
2020-09-05 13:22:54 -0400

    new script

---
+++ packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Autor: PIERRE
+#Date: 09/04/2020
+#Description: install packages in linux(Centos 7)
+
+
+yum install finger -y
+yum install curl -y
+yum install zip -y
+yum install vim -y
+
+exit 0
